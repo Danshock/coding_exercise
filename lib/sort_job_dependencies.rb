fe3840107563b47dabe5bf486c648ef6a2cd15d8
Danislav <danito1233@gmail.com>
2019-07-08 23:50:44 +0100

    I2: When given 1 job, it returns a single job

--- lib/sort_job_dependencies.rb
+++ lib/sort_job_dependencies.rb
@@ -1,5 +1,24 @@
 class SortJobDependencies
 
+	# initialize with a default value = []
 	def initialize
+		@dependencies = Hash.new { |j, d| j[d] = [] }
+	end
+
+	def add_job_dependency(job, *job_dependencies)
+		@dependencies[job] = job_dependencies
+	end
+
+	def jobs_splitter(unsorted_job_dependencies)
+		sorted_job_dds = unsorted_job_dependencies.split(", ")
+		sorted_job_dds.each do |s|
+			k, v = s.split(" => ")
+			@d = add_job_dependency(k, v)
+		end
+	end
+
+	def sort_jobs(string_of_jobs)
+		sort_job_dependencies = SortJobDependencies.new
+		sort_job_dependencies.jobs_splitter(string_of_jobs).join
 	end
 end
--- spec/sort_job_dependencies_spec.rb
+++ spec/sort_job_dependencies_spec.rb
@@ -1,12 +1,13 @@
 require "sort_job_dependencies"
 
 describe SortJobDependencies do
+  describe ".sort_jobs" do
 	let(:sort_job_dependencies) { SortJobDependencies.new }
 
 	context "given an empty string" do
 		let(:string_of_jobs) { "" }
 
-		it "should equal zero elements" do
+		it "should equal 0 elements" do
 			expect(string_of_jobs.size).to eql(0)
 		end
 
@@ -18,4 +19,21 @@ describe SortJobDependencies do
 			expect(string_of_jobs.split).to eql([])
 		end
 	end
+
+	context "given the single job \"a =>\"" do
+		let(:string_of_jobs) { "a" }
+
+		it "returns \"a\"" do
+			expect(sort_job_dependencies.sort_jobs(string_of_jobs)).to eq("a")
+		end
+
+		it "should equal 1 element" do
+			expect(string_of_jobs.size).to eq(1)
+		end
+
+		it "is an object of type String" do
+			expect(string_of_jobs).to be_a(String)
+		end
+	end
+  end
 end
