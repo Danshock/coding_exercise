75df0fe727dc74946b3714825a092f7723d91def
Danislav <danito1233@gmail.com>
2019-07-09 09:28:27 +0100

    I3: When given 3 jobs, it returns the correct 3 jobs

--- lib/sort_job_dependencies.rb
+++ lib/sort_job_dependencies.rb
@@ -1,8 +1,20 @@
+require "tsort"
+
 class SortJobDependencies
+	include TSort
 
 	# initialize with a default value = []
 	def initialize
 		@dependencies = Hash.new { |j, d| j[d] = [] }
+	end
+
+	# The below two methods are required in order for TSort to work
+	def tsort_each_node(&block)
+		@dependencies.each_key(&block)
+	end
+
+	def tsort_each_child(job, &block)
+		@dependencies[job].each(&block) if @dependencies.has_key?(job)	
 	end
 
 	def add_job_dependency(job, *job_dependencies)
@@ -19,6 +31,7 @@ class SortJobDependencies
 
 	def sort_jobs(string_of_jobs)
 		sort_job_dependencies = SortJobDependencies.new
-		sort_job_dependencies.jobs_splitter(string_of_jobs).join
+		sort_job_dependencies.jobs_splitter(string_of_jobs)
+		sort_job_dependencies.tsort.join
 	end
 end
--- spec/sort_job_dependencies_spec.rb
+++ spec/sort_job_dependencies_spec.rb
@@ -7,7 +7,7 @@ describe SortJobDependencies do
 	context "given an empty string" do
 		let(:string_of_jobs) { "" }
 
-		it "should equal 0 elements" do
+		it "should equal 0 jobs" do
 			expect(string_of_jobs.size).to eql(0)
 		end
 
@@ -27,12 +27,28 @@ describe SortJobDependencies do
 			expect(sort_job_dependencies.sort_jobs(string_of_jobs)).to eq("a")
 		end
 
-		it "should equal 1 element" do
+		it "should equal 1 job" do
 			expect(string_of_jobs.size).to eq(1)
 		end
 
 		it "is an object of type String" do
 			expect(string_of_jobs).to be_a(String)
+		end
+	end
+
+	context "given a sequence of three jobs \"a =>\", \"b =>\", \"c =>\"" do
+		let(:string_of_jobs) { "a => , b => , c => " }
+
+		it "returns \"abc\"" do
+			expect(sort_job_dependencies.sort_jobs(string_of_jobs)).to eq("abc")
+		end
+
+		it "is an object of type String" do
+			expect(string_of_jobs).to be_a(String)
+		end
+
+		it "should equal 3 jobs" do
+			expect(sort_job_dependencies.sort_jobs(string_of_jobs).size).to eq(3)
 		end
 	end
   end
