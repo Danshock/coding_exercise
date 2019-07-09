1ef4d1812c50dfe8ac587bc795d3c53cdfbfee12
Danislav <danito1233@gmail.com>
2019-07-09 17:15:08 +0100

    Fix indentation

--- lib/sort_job_dependencies.rb
+++ lib/sort_job_dependencies.rb
@@ -1,45 +1,45 @@
 require "tsort"
-
+ 
 class SortJobDependencies
-	include TSort
-
-	def initialize
-		@dependencies = Hash.new { |job, dep| job[dep] = [] }
-	end
-
-	def sort_jobs(string_of_jobs)
-		jobs_splitter(string_of_jobs)
-		tsort.join
-	end
-
-private
-
-	def add_job_dependency(job, *job_dependencies)
-		@dependencies[job] = job_dependencies
-	end
-
-	def jobs_splitter(unsorted_job_dependencies)
-		sorted_job_dds = unsorted_job_dependencies.split(", ")
-		  
-		  sorted_job_dds.each do |jobs_string|
-			  job, dependency = jobs_string.split(" => ")
-			    
-			    if job === dependency
-				  raise "Job #{job} cannot depend on self."
-			    elsif dependency === nil
-				  add_job_dependency(job)
-			    else
-				  add_job_dependency(job, dependency)
-			    end				
-		  end
-	end
-
-	# The two methods below are required in order for TSort to work
-	def tsort_each_node(&block)
-		@dependencies.each_key(&block)
-	end
-
-	def tsort_each_child(job, &block)
-		@dependencies[job].each(&block) if @dependencies.has_key?(job)	
-	end
+  include TSort
+ 
+  def initialize
+    @dependencies = Hash.new { |job, dep| job[dep] = [] }
+  end
+ 
+  def sort_jobs(string_of_jobs)
+    jobs_splitter(string_of_jobs)
+    tsort.join
+  end
+ 
+  private
+ 
+  def add_job_dependency(job, *job_dependencies)
+    @dependencies[job] = job_dependencies
+  end
+ 
+  def jobs_splitter(unsorted_job_dependencies)
+    sorted_job_dds = unsorted_job_dependencies.split(", ")
+         
+    sorted_job_dds.each do |jobs_string|
+      job, dependency = jobs_string.split(" => ")
+               
+      if job === dependency
+        raise "Job #{job} cannot depend on self."
+      elsif dependency === nil
+        add_job_dependency(job)
+      else
+        add_job_dependency(job, dependency)
+      end              
+    end
+  end
+ 
+  # The two methods below are required in order for TSort to work
+  def tsort_each_node(&block)
+    @dependencies.each_key(&block)
+  end
+ 
+  def tsort_each_child(job, &block)
+    @dependencies[job].each(&block) if @dependencies.has_key?(job)
+  end
 end
